bc2088eaad6cd3a5e2a842dda4c76828f0c683cd
Ciro Lo Sapio <lscorionjs@gmail.com>
2026-03-27 10:41:46 +0100

    bump version to v0.2.0

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-import { checkOption, dockerServiceStartsAutomatically, getLastVersion, hasDockerDesktop, isCurrentUserInDockerGroup, isDocker, isOhMyZshInstalled, isWsl, run, selectWsl } from './functions.ts'
-import { dockerEnginePostInstall, installBat, installBottom, installCarootOnWsl, installCtop, installDeno, installDockerEngine, installEnvman, installFnm, installJetBrainsGateway, installLazygit, installMage2Postman, installMagentoCloudCli, installMkcert, installMkcertWin, installOhMyZsh, installOllama, installSig, installSshs, installStarship } from './softwares.ts'
-import { colors } from '@cliffy/ansi/colors'
 import { Checkbox, CheckboxOption, Confirm } from '@cliffy/prompt'
+import { colors } from '@cliffy/ansi/colors'
+import { checkOption, dockerServiceStartsAutomatically, getLastEnvmanVersion, hasDockerDesktop, isCurrentUserInDockerGroup, isDocker, isOhMyZshInstalled, isWsl, run, selectWsl } from './functions.ts'
+import { dockerEnginePostInstall, installBat, installBottom, installCarootOnWsl, installCtop, installDeno, installDockerEngine, installEnvman, installFnm, installJetBrainsGateway, installLazygit, installMage2Postman, installMagentoCloudCli, installMkcert, installMkcertWin, installOhMyZsh, installOllama, installSig, installSshs, installStarship } from './softwares.ts'
 
-export const VERSION = 'v0.1.5'
+export const VERSION = 'v0.2.0'
 
 async function main() {
 	if (await isDocker()) {
