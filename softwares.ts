d7bfecb9ed6035ff4c6995211ed0e14913c8d8b9
Ciro Lo Sapio <lscorionjs@gmail.com>
2026-08-13 22:50:00 +0200

    update bottom installation to version 0.14.8

--- softwares.ts
+++ softwares.ts
@@ -128,7 +128,7 @@ export async function installSshs() {
 export async function installBottom() {
 	// https://github.com/ClementTsang/bottom
 	console.log(info('installing bottom...'))
-	await run(`wget https://github.com/ClementTsang/bottom/releases/download/0.14.7/bottom_0.14.7-1_amd64.deb -O ./bottom_amd64.deb`.split(' '))
+	await run(`wget https://github.com/ClementTsang/bottom/releases/download/0.14.8/bottom_0.14.8-1_amd64.deb -O ./bottom_amd64.deb`.split(' '))
 	await run(`sudo dpkg -i bottom_amd64.deb`.split(' '))
 	await run(`rm -f bottom_amd64.deb`.split(' '))
 	console.log(success(`bottom installed ✔\n`))
